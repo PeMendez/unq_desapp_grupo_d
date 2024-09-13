c930438ecb42d342fec11964ae36b7112fc8aada
Camila Scaglioni <camila.pia.scaglioni@gmail.com>
2024-09-12 22:05:23 -0300

    inicializacion de proyecto spring

---
+++ src/main/kotlin/com/desapp/crypto_exchange/CryptoExchangeApplication.kt
@@ -0,0 +1,11 @@
+package com.desapp.crypto_exchange
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CryptoExchangeApplication
+
+fun main(args: Array<String>) {
+	runApplication<CryptoExchangeApplication>(*args)
+}
---
+++ src/test/kotlin/com/desapp/crypto_exchange/CryptoExchangeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.desapp.crypto_exchange
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CryptoExchangeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
