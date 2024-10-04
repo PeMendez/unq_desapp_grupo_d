b1bc56629ff2ecb400d94e836d04c81a231fe58e
Camila Scaglioni <camila.pia.scaglioni@gmail.com>
2024-10-03 21:47:20 -0300

    modelo de transaccion Intent, price y enums

--- src/main/kotlin/com/desapp/crypto_exchange/model/TransactionIntent.kt
+++ src/main/kotlin/com/desapp/crypto_exchange/model/TransactionIntent.kt
@@ -8,25 +8,33 @@ class TransactionIntent() {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     var id: Long? = null
+
+    @ManyToOne
+    var owner: User? = null
+
+    @ManyToOne
+    var price: Price? = null
+
     var amount : Float? = null
     var operationType: OperationType? = null
     var active: Boolean? = null
     var createdDate: LocalDateTime? = LocalDateTime.now()
-    var argsAmount : Float? = null
-    var price : String? = null
 
     constructor(
+        owner : User?,
+        price : Price?,
         amount : Float?,
         operationType: OperationType?,
-        active: Boolean?,
-        argsAmount : Float?
-
+        active: Boolean? = true
     ): this() {
+        this.owner = owner
+        this.price = price
         this.amount = amount
         this.operationType = operationType
         this.active = active
+    }
 
-
+    fun calculateArsAmount() {
 
     }
 
--- src/main/kotlin/com/desapp/crypto_exchange/model/enums.kt
+++ src/main/kotlin/com/desapp/crypto_exchange/model/enums.kt
@@ -3,3 +3,20 @@ package com.desapp.crypto_exchange.model
 enum class OperationType {
     PURCHASE, SALE
 }
+
+enum class CryptoActive {
+    ALICEUSDT,
+    MATICUSDT,
+    AXSUSDT,
+    AAVEUSDT,
+    ATOMUSDT,
+    NEOUSDT,
+    DOTUSDT,
+    ETHUSDT,
+    CAKEUSDT,
+    BTCUSDT,
+    BNBUSDT,
+    ADAUSDT,
+    TRXUSDT,
+    AUDIOUSDT,
+}
